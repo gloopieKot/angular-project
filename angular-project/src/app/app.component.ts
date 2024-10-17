730981afb2c4b104ba511477a4134bb9ff343dce
gloopieKot <yakushevalika@yandex.ru>
2024-10-17 07:20:34 +0300

    Настроен компонент main-header-search

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {AdsListComponent} from './components/ads-list/ads-list.component';
 import {HeaderComponent} from './components/header/header.component';
+import {MainHeaderSearchComponent} from './components/main-header-search/main-header-search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdsListComponent, HeaderComponent],
+  imports: [RouterOutlet, AdsListComponent, HeaderComponent, MainHeaderSearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- angular-project/src/app/components/main-header-search/main-header-search.component.ts
+++ angular-project/src/app/components/main-header-search/main-header-search.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import {ButtonDirective} from 'primeng/button';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-header-search',
   standalone: true,
-  imports: [],
+  imports: [
+    MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule, ButtonDirective
+  ],
+  // providers: [MessageService],
   templateUrl: './main-header-search.component.html',
   styleUrl: './main-header-search.component.scss'
 })
-export class MainHeaderSearchComponent {
+export class MainHeaderSearchComponent implements OnInit {
+  items: MenuItem[] | undefined;
 
+  // constructor(private router: Router) {
+  // }
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Все категории',
+        icon: 'pi pi-bars',
+        items: [
+          {
+            label: 'категория1',
+            route: '/installation'
+          },
+          {
+            label: 'категория2',
+            url: 'https://angular.io/',
+            items: [
+              {
+                label: 'подкатегория',
+                route: '/installation'
+              }
+            ]
+          },
+          {
+            label: 'Категория2',
+            route: '/configuration'
+            // url: 'https://angular.io/'
+          }
+        ]
+      }
+    ]
+  }
 }
+
+
