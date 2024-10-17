3b12d92b908598f6b4a0be0c9e67535f6c95c3da
gloopieKot <yakushevalika@yandex.ru>
2024-10-17 05:04:08 +0300

    Настроен компонент для вывода рекомендаций

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AdsListComponent} from './components/ads-list/ads-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- angular-project/src/app/components/ads-list/ads-list.component.ts
+++ angular-project/src/app/components/ads-list/ads-list.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { Product } from '@domain/product';
+import { ProductService } from '@service/productservice';
+import { DataViewModule } from 'primeng/dataview';
+import { TagModule } from 'primeng/tag';
+import { RatingModule } from 'primeng/rating';
+import { ButtonModule } from 'primeng/button';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-ads-list',
   standalone: true,
-  imports: [],
+  imports: [
+    DataViewModule,
+    TagModule,
+    RatingModule,
+    ButtonModule,
+    CommonModule,
+    NgOptimizedImage
+  ],
+  providers: [ProductService],
   templateUrl: './ads-list.component.html',
-  styleUrl: './ads-list.component.scss'
+  styleUrls: ['./ads-list.component.scss']
 })
+
+
 export class AdsListComponent {
+  // layout: string = 'list';
+  layout: "grid" = 'grid';
+
+  products!: Product[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts().then((data: Product[]) => {
+      // this.products = data.slice(0, 12);
+      this.products = data;
+    });
+  }
+
+  getSeverity(product: Product) {
+    console.log(product);
+  }
 
 }
+
