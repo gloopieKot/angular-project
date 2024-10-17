068337bdd128bb12fab44bccb000584c1f314843
gloopieKot <yakushevalika@yandex.ru>
2024-10-17 05:25:01 +0300

    Настроен компонент header

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {AdsListComponent} from './components/ads-list/ads-list.component';
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdsListComponent],
+  imports: [RouterOutlet, AdsListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
